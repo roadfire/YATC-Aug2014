38745e67eed6d5893b5386e20c269a3c6a5a9bc0
Josh Brown <josh@roadfiresoftware.com>
2014-08-27 16:08:03 -0400

    added navigation controller, table view, and some static data

---
+++ YATC/YATC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YATC
+//
+//  Created by Josh Brown on 8/27/14.
+//  Copyright (c) 2014 Roadfire Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RFSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RFSAppDelegate class]));
+    }
+}
